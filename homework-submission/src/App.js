277e8acd74536923c4bd079585f5e7ae26f7c5b9
Marie-Christine Desplat <kiki_online@live.se>
2019-05-22 02:02:58 +0200

    Homework week 1
    Missing input for Undone/Done

---
+++ homework-submission/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './components/Main';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ homework-submission/src/components/Main.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PostData from './TasksList.json';
+
+class Main extends Component {
+  state = {
+    list: [
+      { do: 'Get out of bed', date: 'Wed Sep 13 2017' },
+      { do: 'Brush teeth', date: 'Thu Sep 14 2017' },
+      { do: 'Eat breakfast', date: 'Fri Sep 15 2017' },
+    ],
+    tasks: ['id', 'description', 'deadline'],
+  };
+
+  render() {
+    const {} = this.state;
+    return (
+      <div key={1}>
+        <h2>Static List</h2>
+        <ul>
+          {this.state.list.map(std => (
+            <li>
+              {std.do}, {std.date}
+            </li>
+          ))}
+        </ul>
+        <h2>Dynamic List</h2>
+        <ol>
+          {PostData.map((postDetail, index) => {
+            return (
+              <li key={postDetail.id}>
+                <span class="big">{postDetail.description}</span>
+                <br />
+                Deadline: {postDetail.deadline}
+                <br />
+                Status: {postDetail.done}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Main;
